ce9bbad650dca0213affdc0a3cf75a543adf7220
achbani9 <a.chbani@eca-assurances.com>
2025-12-01 18:23:59 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
-<script setup></script>
-
 <template>
-  <div>Hello world</div>
+  <div>Hello world {{ firstName }} {{ lastName }} {{ count }}</div>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+  <div v-show="count > 5">Count is greater than 0</div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { ref } from "vue";
+const firstName = "John";
+const lastName = "Doe";
+const count = ref(0);
+console.log(count, count.value);
+const increment = (event) => {
+  count.value++;
+  console.log(event);
+};
+const decrement = () => {
+  count.value--;
+};
+</script>
+
+<style scoped>
+div {
+  color: red;
+  font-weight: bold;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  padding: 20px;
+  border: 1px solid red;
+  border-radius: 10px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+  transition: all 0.3s ease;
+  &:hover {
+    background-color: blue;
+    color: white;
+  }
+}
+</style>
